d6b4289dc7e9a405f8d6c547934334ff103ab243
carloslrsa <carlos.lrsa@hotmail.com>
2018-11-13 17:57:30 -0500

    prueba de cerradura

--- ControladorApertura.py
+++ ControladorApertura.py
@@ -91,21 +91,34 @@ class PinNumerico():
             
 class Cerradura():
 
+	abierto = false;
+
+
     def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11,GPIO.OUT)
+        #GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(27,GPIO.OUT)
         #se crea el objeto en el pin 11 con 50 Hz
-        self.pwm = GPIO.PWM(11,50)
-        self.Abrir()
-        self.senal()
+        self.pwm = GPIO.PWM(27,50)
 
-    def Abrir(self):
+        #self.anchoPulso = 0.0014
+        #self.ciclo = self.anchoPulso * 50 * 100 # %
+
+        self.ciclo = [7.5,4.5]
+        self.pwm.start(self.ciclo[0])
+
+
+    def prueba(self,i)
+    	self.pwm.ChangeDutyCycle(self.ciclo[i])
+        #self.Abrir()
+        #self.senal()
+
+    #def Abrir(self):
         #prueba : 1.4 milisegundos --> 0.0018 seg
-        self.anchoPulso = 0.0014
-        self.ciclo = self.anchoPulso * 50 * 100 # %
+        #self.anchoPulso = 0.0014
+        #self.ciclo = self.anchoPulso * 50 * 100 # %
 
-    def senal(self):
-        self.pwm.start(self.ciclo)
-        self.senal()
+    #def senal(self):
+    #    self.pwm.start(self.ciclo)
 
     #def Abrir(self):
